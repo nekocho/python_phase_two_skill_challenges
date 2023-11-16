7f942242a3c3d4757c195ca3dc1fe35587f59a8c
Zoe Cho <zoewycho@gmail.com>
2023-11-16 16:08:45 +0000

    Added Testing Classes

---
+++ testing_classes/tests/test_diaryentry.py
@@ -0,0 +1,22 @@
+from lib.diaryentry import *
+
+def test_if_title_and_contents_is_string():
+    diary = DiaryEntry("test", "contents test")
+    assert diary.title == "test"
+    assert diary.contents == "contents test"
+
+def test_format():
+    diary = DiaryEntry("test", "contents test")
+    assert diary.format() == "test: contents test"
+
+def test_count_words():
+    diary = DiaryEntry("test", "contents test")
+    assert diary.count_words() == 2
+
+def test_reading_time():
+    diary = DiaryEntry("test", "contents test")
+    assert diary.reading_time(2) == 1
+
+def test_reading_chunk():
+    diary = DiaryEntry("test", "contents test")
+    assert diary.reading_chunk(1, 1) == "contents"
